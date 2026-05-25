9058ab87e8e66f04adf840d3eba3047e1d629b8b
Mahmoud <mahmoudhussien743@gmail.com>
2026-05-26 02:36:47 +0300

    Add Swagger

--- Provider/Neighborhood-Services/Neighborhood.Services.API/Program.cs
+++ Provider/Neighborhood-Services/Neighborhood.Services.API/Program.cs
@@ -11,10 +11,18 @@ namespace Neighborhood.Services.API
 
             builder.Services.AddControllers();
             builder.Services.AddSignalR();
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
 
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
+            if(app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
 
             app.UseHttpsRedirection();
 
