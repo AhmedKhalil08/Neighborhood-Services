2664664464d8e277e454c18a15e1c6d6cee3df24
Ziad Gad <ziadmohamedgad@gmail.com>
2026-05-31 13:31:16 +0300

    promocodes dtos, commands, queries implemented

---
+++ Provider/Neighborhood-Services/Neighborhood.Services.Application/PromoCodes/Commands/ApplyPromoCode/ApplyPromoCodeCommandHandler.cs
@@ -0,0 +1,61 @@
+﻿using MediatR;
+using Neighborhood.Services.Application.PromoCodes.DTOs;
+using Neighborhood.Services.Application.PromoCodes.Interface;
+using Neighborhood.Services.Application.Shared;
+using Neighborhood.Services.Domain.PromoCodes;
+namespace Neighborhood.Services.Application.PromoCodes.Commands.ApplyPromoCode
+{
+    public class ApplyPromoCodeCommandHandler : IRequestHandler<ApplyPromoCodeCommand, PromoCodeResponseDto>
+    {
+        private readonly IPromoCodeRepository _promoCodeRepository;
+        private readonly IPromoCodeUsageRepository _promoCodeUsageRepository;
+        private readonly IUnitOfWork _unitOfWork;
+
+        public ApplyPromoCodeCommandHandler(IPromoCodeRepository promoCodeRepository, IPromoCodeUsageRepository promoCodeUsageRepository, IUnitOfWork unitOfWork)
+        {
+            _promoCodeRepository = promoCodeRepository;
+            _promoCodeUsageRepository = promoCodeUsageRepository;
+            _unitOfWork = unitOfWork;
+        }
+
+        public async Task<PromoCodeResponseDto> Handle(ApplyPromoCodeCommand request, CancellationToken cancellationToken)
+        {
+            var isValid = await _promoCodeRepository.IsValidAsync(request.Code);
+
+            if (!isValid)
+                throw new InvalidOperationException($"Promo code '{request.Code}' is invalid or expired.");
+
+            var promoCode = await _promoCodeRepository.GetByCodeAsync(request.Code)
+                ?? throw new KeyNotFoundException($"Promo code '{request.Code}' not found.");
+
+            var alreadyUsed = await _promoCodeUsageRepository.HasUserUsedPromoAsync(request.UserId, promoCode.Id);
+
+            if (alreadyUsed)
+                throw new InvalidOperationException($"Your have already used this promo code.");
+
+            var usage = new PromoCodeUsage
+            {
+                PromoCodeId = promoCode.Id,
+                UserId = request.UserId,
+                BookingId = request.BookingId,
+                UsedAt = DateTime.UtcNow
+            };
+
+            await _promoCodeUsageRepository.AddAsync(usage);
+            await _promoCodeRepository.IncrementUsageAsync(promoCode.Id);
+            await _unitOfWork.SaveChangesAsync(cancellationToken);
+
+            return new PromoCodeResponseDto
+            {
+                Id = promoCode.Id,
+                Code = promoCode.Code,
+                DiscountPercentage = promoCode.DiscountPercentage,
+                MaxUses = promoCode.MaxUses,
+                UsedCount = promoCode.UsedCount,
+                ExpiresAt = promoCode.ExpiresAt,
+                IsActive = promoCode.IsActive,
+                CreatedAt = DateTime.UtcNow
+            };
+        }
+    }
+}
---
+++ Provider/Neighborhood-Services/Neighborhood.Services.Application/PromoCodes/Commands/CreatePromoCode/CreatePromoCodeCommandHandler.cs
@@ -0,0 +1,52 @@
+﻿using MediatR;
+using Neighborhood.Services.Application.PromoCodes.DTOs;
+using Neighborhood.Services.Application.PromoCodes.Interface;
+using Neighborhood.Services.Application.Shared;
+using Neighborhood.Services.Domain.PromoCodes;
+namespace Neighborhood.Services.Application.PromoCodes.Commands.CreatePromoCode
+{
+    public class CreatePromoCodeCommandHandler : IRequestHandler<CreatePromoCodeCommand, PromoCodeResponseDto>
+    {
+        private readonly IPromoCodeRepository _promoCodeRepository;
+        private readonly IUnitOfWork _unitOfWork;
+
+        public CreatePromoCodeCommandHandler(IPromoCodeRepository promoCodeRepository, IUnitOfWork unitOfWork)
+        {
+            _promoCodeRepository = promoCodeRepository;
+            _unitOfWork = unitOfWork;
+        }
+
+        public async Task<PromoCodeResponseDto> Handle(CreatePromoCodeCommand request, CancellationToken cancellationToken)
+        {
+            var exists = await _promoCodeRepository.GetByCodeAsync(request.Code);
+
+            if (exists is not null)
+                throw new InvalidOperationException($"Promo Code with code {request.Code} already exists.");
+
+            var promoCode = new PromoCode
+            {
+                Code = request.Code,
+                DiscountPercentage = request.DiscountPercentage,
+                MaxUses = request.MaxUses,
+                ExpiresAt = request.ExpiresAt,
+                IsActive = true,
+                UsedCount = 0
+            };
+
+            await _promoCodeRepository.AddAsync(promoCode);
+            await _unitOfWork.SaveChangesAsync(cancellationToken);
+
+            return new PromoCodeResponseDto
+            {
+                Id = promoCode.Id,
+                Code = promoCode.Code,
+                DiscountPercentage = promoCode.DiscountPercentage,
+                MaxUses = promoCode.MaxUses,
+                UsedCount = promoCode.UsedCount,
+                ExpiresAt = promoCode.ExpiresAt,
+                IsActive = promoCode.IsActive,
+                CreatedAt = promoCode.CreatedAt,
+            };
+        }
+    }
+}
